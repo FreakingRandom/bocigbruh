cf66a642ccaf116766a3845fe14e9978d21d4adc
BuildTools <unconfigured@null.spigotmc.org>
2021-01-26 18:05:30 +0100

    bocig 0.1.2

--- commands/jakajestpogoda.js
+++ commands/jakajestpogoda.js
@@ -1,22 +1,25 @@
 const discord = require("discord.js")
 module.exports.run = (msg,bot,args)=>{
-    const luck = Math.round(Math.random()*4)+1
+    const luck = Math.round(Math.random()*5)+1
     console.log(luck)
     switch(luck){
         case 1:
-            msg.channel.send("Przewidywane opady gradu") 
+            msg.channel.send("Przewidywane opady gradu :cloud_rain:") 
             break;
         case 2:
-            msg.channel.send("Będzie słonecznie") 
+            msg.channel.send("Będzie słonecznie :sunny:") 
             break;
         case 3:
-            msg.channel.send("Przewidywane tornado. Polecamy się schronić")
+            msg.channel.send("Przewidywane tornado. Polecamy się schronić :cloud_tornado:")
             break;
         case 4:
-            msg.channel.send("Przewidywane opady deszczu")
+            msg.channel.send("Przewidywane opady deszczu :cloud_rain:")
             break;
         case 5:
-            msg.channel.send("Będzie pochmurno")
+            msg.channel.send("Będzie pochmurno :cloud:")
+            break;
+        case 6:
+            msg.channel.send("Będzie padać śnieg :cloud_snow:")
             break;
         default:
             msg.channel.send("Coś poszło nie tak")
