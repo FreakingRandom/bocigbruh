e22a2c6d01a7069b9e05696ff0e25b15c8c5ac4b
BuildTools <unconfigured@null.spigotmc.org>
2021-02-26 12:09:49 +0100

    rr update from hell 0.1.6

--- commands/shop.js
+++ commands/shop.js
@@ -1,40 +1,41 @@
-const discord = require("discord.js")
-const userdata = require("../userdata.js")
-const rankup = require("../rankup.js")
+const discord = require('discord.js');
+const userdata = require('../userdata.js');
+const rankup = require('../rankup.js');
 module.exports.run = (msg, bot, args) => {
-    let zczytywanko = userdata.getUserData(msg.member.id)
-    let money = zczytywanko.money
+    		let zczytywanko = userdata.getUserData(msg.member.id);
+    		let money = zczytywanko.money;
 
     //args.length długość argumentów
-    if (args.length == 0) {
-        const embed = new discord.MessageEmbed()
-        embed.setTitle("Sklepik :shopping_cart:")
-        embed.addField("1 Rank-up  :five::zero::zero::zero: :dollar:","Dostajesz range wyżej")
-        embed.addField("2 Bilet na loterie  :one::zero::dollar:","W.I.P")
-        embed.setColor("#cc2c16")
-        msg.channel.send(embed);
+    		if (args.length == 0) {
+        		const embed = new discord.MessageEmbed();
+        		embed.setTitle('Sklepik :shopping_cart:');
+        		embed.addField('1 Rank-up  :five::zero::zero::zero: :dollar: ','Dostajesz range wyżej');
+        		embed.addField('2 Bilet na loterie  :one::zero::dollar: ','W.I.P');
+        		embed.addField('3 Koparka kryptowalut :nine::nine::nine::nine::nine::nine::dollar:- ','W.I.P');
+        		embed.setColor('#cc2c16');
+        		msg.channel.send(embed);
     }
     else{
-        console.log(args[0])
-        switch (args[0]) {
-            case "1":
-                if (money >= 5000) {
-                    userdata.setUserData(msg.member.id,"money",money - 5000)
-                    const ranga = rankup.rankup(msg.member)
-                    const ranganame = ranga.name
-                    setTimeout(()=>{
-                        msg.channel.send(`Zakupiłeś ${ranganame}.Twój balans wynosi teraz ${userdata.getUserData(msg.member.id).money}.`);
+        		console.log(args[0]);
+        		switch (args[0]) {
+            case '1':
+                		if (money >= 500) {
+                    		userdata.setUserData(msg.member.id,'money',money - 5000);
+                    		const ranga = rankup.rankup(msg.member);
+                    		const ranganame = ranga.name;
+                    		setTimeout(()=>{
+                        		msg.channel.send(`Zakupiłeś ${ranganame}.Twój balans wynosi teraz ${userdata.getUserData(msg.member.id).money}.`);
                     },20);
-                    break;
+                    		break;
                 } else {
-                    msg.channel.send("Nie masz wystarczająco pieniędzy")
+                    		msg.channel.send('Nie masz wystarczająco pieniędzy');
                 }
         }
     }
-}
+};
 
 
 module.exports.help = {
-    "name": "Shop",
-    "description": "Pokazuje towary dostępne w sklepie. `Shop"
-}
+    	'name': 'Shop',
+    	'description': 'Pokazuje towary dostępne w sklepie. `Shop'
+};
