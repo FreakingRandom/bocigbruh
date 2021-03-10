6e6aa7c9e1b72da6df1d994b6a363fc12cb466d6
Borys Moder <bory.mode@o2.pl>
2021-03-10 12:52:50 +0100

    crypto update 0.2.0

--- commands/koparka.js
+++ commands/koparka.js
@@ -1,16 +1,26 @@
 const discord = require("discord.js")
 const userdata = require("../userdata.js")
-module.exports.run=(msg,args,bot)=>{
+module.exports.run=(msg,bot,args)=>{
     const zczytywansko = userdata.getUserData(msg.author.id)
     const LastMined = new Date(zczytywansko.LastMined) || new Date()
     const lvl = zczytywansko.koparka || 0
     const czasuplyniety = (LastMined.getTime() - new Date().getTime())/1000
+    const money = userdata.getUserData(msg.author.id).money
     if(lvl==0){
         msg.channel.send(`Do użycia tej komendy wymagana jest koparka`)
     }
     else {
-        const iloscpieniedzywkoparce = 500 + czasuplyniety*(czasuplyniety/(Math.pow(2,10)))
-        msg.channel.send(`Masz w koparce zgromadzone ${iloscpieniedzywkoparce} :dollar:`)
+        const iloscpieniedzywkoparce = Math.round(500 + czasuplyniety*(czasuplyniety/(Math.pow(2,10/lvl))))
+        switch(args[0]){
+            case 'collect':
+            userdata.setUserData(msg.author.id,"LastMined", new Date().toString())
+            userdata.setUserData(msg.author.id, "money", money + iloscpieniedzywkoparce)
+            msg.channel.send(`Zarobiłeś ${iloscpieniedzywkoparce} :dollar: kopiąc kryptowalutę.`)
+            break;
+            default:
+                msg.channel.send(`Masz w koparce zgromadzone ${iloscpieniedzywkoparce} :dollar:`)
+                break;
+        }
     }
     
 }
